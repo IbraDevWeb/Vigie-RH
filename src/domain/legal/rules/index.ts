63ba3c4ac7d04f2ddbc80250a249fa2e9f1548d1
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-09-09 12:47:56 +0200

    feat(renewal): register renewal rule set

--- src/domain/legal/rules/index.ts
+++ src/domain/legal/rules/index.ts
@@ -3,6 +3,7 @@ import { baseRules } from "./base.rules";
 import { permitRules } from "./permit.rules";
 import { employmentRules } from "./employment.rules";
 import { studentFormalityRules } from "./student-formality.rules";
+import { renewalRules } from "./renewal.rules";
 import { actionRules } from "./action.rules";
 
 export const legalRules: LegalRule[] = [
@@ -10,5 +11,6 @@ export const legalRules: LegalRule[] = [
   ...permitRules,
   ...employmentRules,
   ...studentFormalityRules,
+  ...renewalRules,
   ...actionRules,
 ].sort((a, b) => b.priority - a.priority);
