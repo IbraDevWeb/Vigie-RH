30ce65c11706b0b20d9ed1183dd6f0b9d099afd6
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-09-09 11:16:24 +0200

    feat(legal): register student hiring formality rule

--- src/domain/legal/rules/index.ts
+++ src/domain/legal/rules/index.ts
@@ -2,7 +2,13 @@ import type { LegalRule } from "../types";
 import { baseRules } from "./base.rules";
 import { permitRules } from "./permit.rules";
 import { employmentRules } from "./employment.rules";
+import { studentFormalityRules } from "./student-formality.rules";
 import { actionRules } from "./action.rules";
 
-export const legalRules: LegalRule[] = [...baseRules, ...permitRules, ...employmentRules, ...actionRules]
-  .sort((a, b) => b.priority - a.priority);
+export const legalRules: LegalRule[] = [
+  ...baseRules,
+  ...permitRules,
+  ...employmentRules,
+  ...studentFormalityRules,
+  ...actionRules,
+].sort((a, b) => b.priority - a.priority);
